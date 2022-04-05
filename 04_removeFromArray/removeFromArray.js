5354051b3ac308ceb103d41cfacbf1b479f37b66
bryantconrad <bryant.conradl@gmail.com>
2022-04-05 11:29:27 -0400

    complete 04_removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,15 @@
-const removeFromArray = function() {
+const removeFromArray = function (initialArray /**/ ) {
+    // accept an initial array and arguments that might be elements in the array
+    // find the index of the element that matches each argument in the initial array
+    // remove the elements at those indexes
+    // return the new array
 
-};
+    for (let i = 1; i < arguments.length; i++) {
+        let indexToRemove = initialArray.indexOf(arguments[i]);
+        if (indexToRemove != -1) initialArray.splice(indexToRemove, 1);
+    }
+    return initialArray;
+}
 
 // Do not edit below this line
 module.exports = removeFromArray;
