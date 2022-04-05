35dfa98e94545b196cebb42e099d8d3a69deb7d8
bryantconrad <bryant.conradl@gmail.com>
2022-04-04 23:28:00 -0400

    complete 03_reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,14 @@
-const reverseString = function() {
+const reverseString = function(inputText) {
+// Get the last character of the string
+// Write it as the first character in a new string
+// Repeat for the second to last character, continue looping backwards
+    let reversedText = "";
+    for (let i = inputText.length-1; i >= 0; i--) {
+        reversedText += inputText[i];
+    }
+    return reversedText;
 
+// Shorter solution: return string.split('').reverse().join('');
 };
 
 // Do not edit below this line
