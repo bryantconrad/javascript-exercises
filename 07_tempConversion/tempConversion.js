e084a577ffce085a3cd8dc41c2f3bb5e65cf3f0e
bryantconrad <bryant.conradl@gmail.com>
2022-04-05 15:00:36 -0400

    complete 07_tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,9 @@
-const ftoc = function() {
-
+const ftoc = function(inputTemp) {
+  return parseFloat(((inputTemp - 32) * (5/9)).toFixed(1));
 };
 
-const ctof = function() {
-
+const ctof = function(inputTemp) {
+  return parseFloat(((inputTemp * (9/5)) + 32).toFixed(1));
 };
 
 // Do not edit below this line
